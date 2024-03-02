460e0ac29d05c1c8cf4360de164e41dbc19ef3be
patan3 <62438886+patan3@users.noreply.github.com>
2024-03-02 12:19:16 +0100

    Functioning draft version

--- Template1_2dCharacter/src/Enemy/EnemyTmp.gd
+++ Template1_2dCharacter/src/Enemy/EnemyTmp.gd
@@ -2,7 +2,6 @@ extends Area2D
 
 signal player_entered
 
-export var speed = Vector2(400,400)
 
 onready var _initial_position: = global_position
 
@@ -10,14 +9,19 @@ onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 
 
 export var is_active: bool = true setget set_is_active
+export var speed_h_array: PoolRealArray
+
+var speed: Vector2
 
 func _ready():
 	connect("area_entered", self, "_on_EnemyTmp_area_entered")
 	animation_player.connect("animation_finished", self, "_on_AnimationPlayer_animation_finished")
+	speed.y = 0.0
+	speed.x = Utils.choose(speed_h_array)
 
 
 func _physics_process(delta):
-	position += _initial_position + speed * delta
+	position +=  speed * delta
 
 
 func _on_AnimationPlayer_animation_finished(anim_name: String):
@@ -26,8 +30,9 @@ func _on_AnimationPlayer_animation_finished(anim_name: String):
 
 func _on_EnemyTmp_area_entered(area: Area2D):
 	if area.name == "EnemyDetector":
+		animation_player.play("flip")
 		emit_signal("player_entered")
-		is_active = false
+		set_is_active(false)
 
 func set_is_active(value: bool):
 	is_active = value
