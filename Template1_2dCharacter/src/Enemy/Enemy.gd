75a85b9f6d6259a9783ea0dbd0ae20bca38a8075
upiny-0 <pierredelaet2000@gmail.com>
2024-03-03 16:25:14 +0100

    stop bug

--- Template1_2dCharacter/src/Enemy/Enemy.gd
+++ Template1_2dCharacter/src/Enemy/Enemy.gd
@@ -7,13 +7,18 @@ var direction
 onready var _initial_position: = global_transform.origin
 
 onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
-#onready var 
-
+onready var skin: MeshInstance = get_node("Position3D/elvis/elvis")
+onready var redTimer: Timer = get_node("RednessTimer")
 
 export var is_active: bool = true setget set_is_active
 
 func _ready():
 	animation_player.connect("animation_finished", self, "_on_AnimationPlayer_animation_finished")
+
+
+func _physics_process(delta):
+	pass
+	#skin.material_overlay
 
 
 func _on_AnimationPlayer_animation_finished(anim_name: String):
