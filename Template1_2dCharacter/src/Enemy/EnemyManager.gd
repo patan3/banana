18af6246a2ffdbdb1a52191682d24838b25723a8
upiny-0 <pierredelaet2000@gmail.com>
2024-03-03 06:24:05 +0100

    wqves

--- Template1_2dCharacter/src/Enemy/EnemyManager.gd
+++ Template1_2dCharacter/src/Enemy/EnemyManager.gd
@@ -1,27 +1,23 @@
 extends Node2D
 
-export var startSpawnInterval = 1.0
-export var difficultyIncreaseTimer = 5.0
-export var difficultyIncreaseStep = 1.1
-export var minSpawnInterval = 1.0
+onready var waves : Array = find_waves()
+var betweenWaves = false
+var waveIndex : int = 0
 
 var enemy = preload("res://Template1_2dCharacter/src/Enemy/Enemy.tscn")
 
-onready var timer = get_node("Timer")
-
-var time_elapsed : float = 0.0
+onready var timer = get_node("SpawnTimer")
+onready var timerWave = get_node("WaveTimer")
 
 enum SIDE {TOP, RIGHT, LEFT, BOTTOM}
-
-func _physics_process(delta):
-	time_elapsed += delta
-	if time_elapsed > difficultyIncreaseTimer && timer.wait_time > minSpawnInterval:
-		timer.wait_time /= difficultyIncreaseStep
-		time_elapsed = 0.0
 	
 func _ready():
-	timer.wait_time = startSpawnInterval
 	timer.connect("timeout", self, "spawn")
+	timerWave.connect("timeout", self, "wave_end")
+	if waves.empty():
+		return
+	timer.wait_time = waves[waveIndex].spawnInterval
+	timerWave.wait_time = waves[waveIndex].waveDuration
 
 func generate_position():
 	var size = get_viewport().size
@@ -38,9 +34,33 @@ func generate_position():
 		SIDE.RIGHT:
 			return Vector2(size.x, rand_range(0.0, size.y))
 
+
+func wave_end():
+	
+	if !betweenWaves:
+		betweenWaves = true
+		timerWave.start(3.0)
+		timer.stop()
+	else:
+		waveIndex += 1
+		print("waves: " + str(waveIndex + 1))
+		if waveIndex >= waves.size():
+			printerr("NO MORE WAVES")
+			return
+		betweenWaves = false
+		timer.start(waves[waveIndex].spawnInterval)
+		timerWave.start(waves[waveIndex].waveDuration)
+
+
 func spawn():
 	var instance = enemy.instance()
 	instance.position = generate_position()
 	instance.direction = ceil(((get_viewport().size / 2) - instance.position).x)/(get_viewport().size.x/2)
 	self.add_child(instance)
 
+func find_waves():
+	var all_waves = []
+	for child in get_children():
+		if child is Wave:
+			all_waves.append(child)
+	return all_waves
---
+++ Template1_2dCharacter/src/Enemy/Wave.gd
@@ -0,0 +1,6 @@
+extends Node
+
+class_name Wave
+
+export var spawnInterval : float = 4.0
+export var waveDuration : float = 10.0
